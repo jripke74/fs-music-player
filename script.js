48de6c4c899f3439a74a6539b6851002c9920fb4
Jeff Ripke <jripke74@gmail.com>
2025-04-16 08:46:09 -0700

    add variables

---
+++ script.js
@@ -0,0 +1,22 @@
+// Step 1
+// In this workshop you will create a music player app capable of
+// playing, pausing, and skipping songs from a playlist.
+
+// The HTML and CSS of this project have been provided for you,
+// so you can focus on the JavaScript.
+
+// Start by accessing the #playlist-songs, #play, and #pause elements
+// and assign them to the variables playlistSongs, playButton and
+// pauseButton, respectively.
+
+// Step 2
+// Access the #next and #previous elements from the HTML file.
+
+// Assign them to variables named nextButton, and previousButton,
+// respectively.
+
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("nest");
+const previousButton = document.getElementById("previous");
